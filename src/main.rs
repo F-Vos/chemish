e2e7af14c871fb4dd432e32e4d1c20d9793db1d2
F-Vos <frankvos61@gmail.com>
2025-03-30 21:01:57 +0200

    main structure and window reaction

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use app::App;
+use winit::event_loop::EventLoop;
+
+mod app;
+
+fn main() -> Result<()> {
+    tracing_subscriber::fmt::init();
+    let mut app = App::default();
+    let event_loop = EventLoop::new()?;
+    event_loop.run_app(&mut app)?;
+    Ok(())
 }
